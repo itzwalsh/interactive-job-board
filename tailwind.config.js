63223d20d0d95a82a1057d1d8ecef947b7e8e41f
Zack Walsh <itzwalsh@gmail.com>
2023-08-01 11:28:15 -0400

    initial commit of frontend interactive job board

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      mobile: "375px",
+      desktop: "1440px",
+    },
+    colors: {
+      "primary-cyan": "hsl(180, 29%, 50%)",
+      "neutral-cyan-background": "hsl(180, 52%, 96%)",
+      "neutral-cyan-card": "hsl(180, 31%, 95%)",
+      "neutral-dark-blue": "hsl(180, 8%, 52%)",
+      "neutral-dark-blue-text": "hsl(180, 14%, 20%)",
+    },
+    fontFamily: {
+      spartan: ["Spartan", "sans-serif"],
+    },
+    extend: {
+      fontSize: {
+        title: "15px;",
+      },
+    },
+  },
+  plugins: [],
+}
